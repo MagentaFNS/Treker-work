7c5346d6ef2c48cee5ab6097cd5d238c36f864b5
gleb <glebpolivoda@gmail.com>
2026-03-28 23:45:28 +0300

    наконец-то доделал иконку

--- Treker-work.py
+++ Treker-work.py
@@ -1,5 +1,7 @@
 import customtkinter as ctk
 import os
+from PIL import Image, ImageTk
+
 
 class CleanSlateApp:
     def __init__(self):
@@ -9,8 +11,14 @@ class CleanSlateApp:
         self.app.title("Tracker Work")
         self.app.attributes("-fullscreen", True)
         self.app.configure(fg_color="black")  # Черный фон
-        
         self.app.attributes("-alpha", 0.90)
+
+                # ===== ИКОНКА =====
+        icon_path = r"C:\Users\Глеб\Documents\мой трекер\Tracker\Tracker\icon.ico"
+        if os.path.exists(icon_path):
+            self.app.iconbitmap(icon_path)
+        else:
+            print("Иконка не найдена:", icon_path)
 
         # Привязка клавиш
         self.app.bind("<F11>", self.toggle_fullscreen)
@@ -25,6 +33,7 @@ class CleanSlateApp:
 
     def run(self):
         self.app.mainloop()
+
 
 if __name__ == "__main__":
     app = CleanSlateApp()
